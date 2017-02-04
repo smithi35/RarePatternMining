fc5de8c30c6a110bdffeecc948594aadf0b9afe6
Ian Smith <ian_smith_12a@mymts.net>
2017-02-03 22:46:51 -0600

    Patterns now successfully reads in a transaction database represented by a txt file

--- SourceCode/Patterns.cpp
+++ SourceCode/Patterns.cpp
@@ -1,16 +1,48 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
 
 using namespace std;
 
-string get_contents(string filename)
+string get_contents(const char* filename)
 {
-	return "";
+	ifstream stream;
+	stream.open(filename);
+	stream.seekg(0, ios::end);
+	
+	int length = stream.tellg();
+	stream.seekg(0, ios::beg);
+	
+	char buffer[length+1] = {0};
+	
+	if (buffer != NULL)
+	{
+		stream.read(buffer, length);
+	}
+	else 
+	{
+		cout << "Not enough memory for input file" << endl;
+	}
+	stream.close();
+	
+	return buffer;
+}
+
+void process(const char *inputfilename, const char *outputfilename)
+{
+	string contents = get_contents(inputfilename);
+	cout << contents << endl;
 }
 
 int main()
 {
-	string filename = "PreciseDB.txt";
-	string contents = get_contents(filename);
+	string input = "PreciseDB.txt";
+	const char *inputfilename = input.c_str();
+	string output = "first_trial.txt";
+	const char *outputfilename = output.c_str();
+	process(inputfilename, outputfilename);
 	
 	return 0;
 }
