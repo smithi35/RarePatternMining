f8a8576fb9011628532434dc29a798c55f429e18
Ian Smith <ian_smith_12a@mymts.net>
2017-02-03 22:41:10 -0600

    Added some very basic code. The first task will be reading the transaction database.

---
+++ SourceCode/Patterns.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+string get_contents(string filename)
+{
+	return "";
+}
+
+int main()
+{
+	string filename = "PreciseDB.txt";
+	string contents = get_contents(filename);
+	
+	return 0;
+}
