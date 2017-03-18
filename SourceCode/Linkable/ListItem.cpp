850f4fea128fd1c65048e791061bcb484d23de76
Ian Smith <ian_smith_12a@mymts.net>
2017-03-18 12:19:47 -0500

    Was just testing out the virtual destructor to make sure it works

--- SourceCode/Linkable/ListItem.cpp
+++ SourceCode/Linkable/ListItem.cpp
@@ -4,4 +4,4 @@ void ListItem::set_support(int s) { support = s; }
 int ListItem::get_support() { return support; }
 void ListItem::increment_support() { support++; }
 void ListItem::increase_support(int add) { support += add; }
-ListItem::~ListItem() {}
+ListItem::~ListItem() { }
